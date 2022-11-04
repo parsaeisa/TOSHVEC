a6033b3fb66454c859a85f67d4d30f2ae8b29c7f
parsaeisa <parsaeisa@github.com>
2022-11-04 17:39:46 +0330

    read values from arguments and add lengths

---
+++ environment/transitions.py
@@ -0,0 +1,11 @@
+
+class Transitions:
+    def __init__(self, v2v_communication_links_available,
+                 v2v_communication_links_bandwidth,
+                 v2r_communication_links_available,
+                 v2r_communication_links_bandwidth):
+        self.V2V0_communication_links_available = v2v_communication_links_available
+        self.V2V0_communication_links_bandwidth = v2v_communication_links_bandwidth
+
+        self.V2R_communication_links_available = v2r_communication_links_available
+        self.V2R_communication_links_bandwidth = v2r_communication_links_bandwidth
