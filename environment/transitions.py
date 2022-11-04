eb5c3287afdbd72f88e67b72618fc23373145efa
parsaeisa <parsaeisa@github.com>
2022-11-04 17:48:44 +0330

    add trans rate

--- environment/transitions.py
+++ environment/transitions.py
@@ -2,10 +2,14 @@
 class Transitions:
     def __init__(self, v2v_communication_links_available,
                  v2v_communication_links_bandwidth,
+                 v2v_communication_links_trans_rate,
                  v2r_communication_links_available,
-                 v2r_communication_links_bandwidth):
+                 v2r_communication_links_bandwidth,
+                 v2r_communication_links_trans_rate):
         self.V2V0_communication_links_available = v2v_communication_links_available
         self.V2V0_communication_links_bandwidth = v2v_communication_links_bandwidth
+        self.v2v_communication_links_trans_rate = v2v_communication_links_trans_rate
 
         self.V2R_communication_links_available = v2r_communication_links_available
         self.V2R_communication_links_bandwidth = v2r_communication_links_bandwidth
+        self.v2r_communication_links_trans_rate = v2r_communication_links_trans_rate
