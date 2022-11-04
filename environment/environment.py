6852ce8faac9041c980b2658330bd42194e3b33f
parsaeisa <parsaeisa@github.com>
2022-11-04 17:40:04 +0330

    read values from arguments and add lengths

--- environment/environment.py
+++ environment/environment.py
@@ -1,7 +1,16 @@
 class Environment:
-    def __init__(self):
+    def __init__(self, rsus, mission_vehicles, cooperative_vehicles, tasks):
         # Put a matrix for bandwidths - between each two RSUs
-        self.RSUs = []
+        self.RSUs = rsus
 
         # vehicles
+        self.mission_vehicles = mission_vehicles
+        self.cooperative_vehicles = cooperative_vehicles
+
         # tasks
+        self.tasks = tasks
+
+        # adjust numbers
+        self.RSU_COUNT = len(self.RSUs)
+        self.M = len(mission_vehicles)
+        self.J = len(cooperative_vehicles)
