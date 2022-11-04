412d4bed6b9938b63c754cd27bb62e5b3f1fd960
parsaeisa <parsaeisa@github.com>
2022-11-04 17:58:41 +0330

    add computation costs

--- environment/RSU.py
+++ environment/RSU.py
@@ -1,3 +1,17 @@
 class RSU:
     def __init__(self, radius):
         self.radius = radius
+
+    # Computation costs
+    # this is used when deciding about offloading scheme
+    # execute locally or offload ??
+    def locally_execution_cost(self, data):
+        return self._computation_cost(data, self.mobile_process_capable)
+
+    def mec_execution_cost(self, data):
+        return self._computation_cost(data, self.mec_process_capble)
+
+    def _computation_cost(self, data, processing_power):
+        computation_time = data / processing_power
+
+        return computation_time
