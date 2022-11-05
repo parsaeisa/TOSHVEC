70f1182bebe00d3538f4e7e580cc963675633696
parsaeisa <parsaeisa@github.com>
2022-11-05 10:45:05 +0330

    add transmissions

--- environment/environment.py
+++ environment/environment.py
@@ -6,7 +6,7 @@ class Environment:
         A singleton object
     """
 
-    def __init__(self, rsus, mission_vehicles, cooperative_vehicles, tasks):
+    def __init__(self, rsus, mission_vehicles, cooperative_vehicles, tasks, transmissions):
         # Put a matrix for bandwidths - between each two RSUs
         self.RSUs = rsus
 
@@ -26,3 +26,6 @@ class Environment:
         # coverage matrices
         self.RSU_vehicle_connected = numpy.zeros((self.M, self.RSU_COUNT), int)
         self.mission_cooperative_vehicle_connected = numpy.zeros((self.M, self.J), int)
+
+        # transmissions
+        self.transmissions = transmissions
