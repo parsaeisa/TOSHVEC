36e7769b3abdb308b9548e2baa3d71eea1258722
parsaeisa <parsaeisa@github.com>
2022-11-25 14:31:08 +0330

    trying to define steps

---
+++ main.py
@@ -0,0 +1,18 @@
+# Describing environment
+"""
+define environment
+define transitions' bandwidths
+define cooperative vehicles
+define mission vehicles
+
+"""
+
+# DQN
+"""
+Define a DQN model 
+Call a method to choose the best action
+Call a transition method to perform that action
+Call a method to learn something ( I didn't get that )  
+Call a method to store the transitions 
+
+"""
