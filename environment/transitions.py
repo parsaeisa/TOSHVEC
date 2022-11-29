6c0493b27dfe0be70f5d79ec0c6fbad83a308cf1
parsaeisa <parsaeisa@github.com>
2022-11-29 09:07:37 +0330

    compute processing time

--- environment/transitions.py
+++ environment/transitions.py
@@ -27,11 +27,18 @@ class Transitions:
     def offloading_to_rsu_delay(self):
         pass
 
-    def offloading_to_cooperative_vehicle_delay(self):
-        pass
+    def offloading_to_cooperative_vehicle_delay(self,task, cooperative_vehicle):
+        L_m =
+        r_mt_j =
 
-    def processing_locally_delay(self):
-        pass
+        T_comm = L_m / r_mt_j
+        T_comp = task.C / cooperative_vehicle.f
+
+        T_total = T_comm + T_comp
+        return T_total
+
+    def processing_locally_delay(self,task, mission_vehicle):
+        return task.C / mission_vehicle.f
 
 
 def compute_interference_V2R():
