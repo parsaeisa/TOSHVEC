eb8c10114fdeb60ba845bf0596f9f212a096864f
parsaeisa <parsaeisa@github.com>
2022-12-23 21:42:02 +0330

    define steps by calling corresponding method

--- environment/environment.py
+++ environment/environment.py
@@ -1,6 +1,8 @@
 import numpy
 
 from trajectory_prediction import model
+from offloading_env import OffloadingEnvironment
+from transitions import Transitions
 
 class Environment:
     """
@@ -8,8 +10,7 @@ class Environment:
     """
 
     def __init__(self, rsus, mission_vehicles, cooperative_vehicles, tasks, transmissions, timeslots_count):
-
-        #timelots
+        # timeslots
         self.timeslots_count = timeslots_count
 
         # Put a matrix for bandwidths - between each two RSUs
@@ -35,14 +36,34 @@ class Environment:
         # transmissions
         self.transmissions = transmissions
 
+    def read_from_config(self):
+        # This method reads environment variables from a config.yml file .
+        pass
+
     def start_execution(self):
         for _ in range(self.timeslots_count):
+
+            self.read_from_config()
+            transitions = Transitions(
+                v2v_communication_links_available,
+                v2v_communication_links_bandwidth,
+                v2r_communication_links_available,
+                v2r_communication_links_bandwidth,
+                self.tasks, self.cooperative_vehicles
+            )
             # Read alpha, betta, w1, d_max from a config file or bash command
             # * Config file is preferred .
             vs = model.VehicleSelection(alpha, betta, w1, d_max,
                                         self.mission_vehicles,
-                                        self.cooperative_vehicles)
+                                        self.cooperative_vehicles,
+                                        transitions)
+            # Here you should call some of vs's methods
+            vs.learn_trajectories()
+            vs.compute_distances()
+            vs.cooperative_vehicles()
+            vs.compute_delay()
 
             # now based on these distances we make an array
             # of candidate cooperative vehicles for each mission vehicle to offload
             # their tasks to . ( the distance must be lower than d_max )
+            oe = OffloadingEnvironment()
