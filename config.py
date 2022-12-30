844e9eb98e959e753765340a7f00e6fecb5e81a3
parsaeisa <parsaeisa@github.com>
2022-12-31 02:33:15 +0330

    move reading operations to a better place

--- config.py
+++ config.py
@@ -3,16 +3,14 @@ from yaml.loader import SafeLoader
 
 
 class Config:
-    def __init__(self):
+
+    def init(self):
         with open('config.yaml') as f:
             data = yaml.load(f, Loader=SafeLoader)
 
         self.DQN = DQNConfig()
         self.DQN.lr = data["dqn"]["learning_rate"]
         self.DQN.gamma = data["dqn"]["gamma"]
-
-    def init(self):
-        pass
 
 
 class DQNConfig:
