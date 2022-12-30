dce8afa8a06ec71285b133915dfaf698e44f51af
parsaeisa <parsaeisa@github.com>
2022-12-31 02:38:53 +0330

    add vehicle selection configs

--- config.py
+++ config.py
@@ -12,6 +12,11 @@ class Config:
         self.DQN.lr = data["dqn"]["learning_rate"]
         self.DQN.gamma = data["dqn"]["gamma"]
 
+        self.vs = VehicleSelectionConfig()
+        self.vs.alpha = data["vehicle_selection"]["alpha"]
+        self.vs.betta = data["vehicle_selection"]["betta"]
+        self.vs.w1 = data["vehicle_selection"]["w1"]
+        self.vs.d_max = data["vehicle_selection"]["d_max"]
 
 class DQNConfig:
     def __init__(self):
@@ -24,4 +29,14 @@ class TransmissionConfig:
         pass
 
 
+class VehicleSelectionConfig:
+    def __init__(self):
+        self.alpha = 0
+        self.betta = 0
+        self.w1 = 0
+        self.d_max = 0
+
+
 c = Config()
+
+c.init()
