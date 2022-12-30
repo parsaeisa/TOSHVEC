8ba9a9e3e857ea36b5299c3020d72ffc48d70dea
parsaeisa <parsaeisa@github.com>
2022-12-31 00:33:51 +0330

    a brief implementation on rl , revenue and costs

--- DQN/states.py
+++ DQN/states.py
@@ -13,7 +13,7 @@ class ActionSpace:
     action space is dedicated to each task (??)
     """
 
-    def __init__(self):
-        self.lambda_R_m_t = 0
-        self.lambda_L_m_t = 0
-        self.lambda_V_m_t = 0
+    def __init__(self, lambda_R_m_t, lambda_L_m_t, lambda_V_m_t):
+        self.lambda_R_m_t = lambda_R_m_t
+        self.lambda_L_m_t = lambda_L_m_t
+        self.lambda_V_m_t = lambda_V_m_t
