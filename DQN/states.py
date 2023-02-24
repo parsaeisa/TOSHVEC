649258dd9595af4d87b93d1cb04dbf7253efa643
parsaeisa <parsaeisa@github.com>
2023-02-24 22:20:25 +0330

    write a factory method for state

--- DQN/states.py
+++ DQN/states.py
@@ -1,13 +1,14 @@
 class State:
-    def __init__(self):
+    def __init__(self, G_j_m_t, G_R_m_t,
+                 freq_R_m_t, freq_l_m_t, freq_j_m_t):
         # task transmission
-        self.G_j_m_t = 0
-        self.G_R_m_t = 0
+        self.G_j_m_t = G_j_m_t
+        self.G_R_m_t = G_R_m_t
 
         # task calculation
-        self.freq_R_m_t = 0
-        self.freq_l_m_t = 0
-        self.freq_j_m_t = 0
+        self.freq_R_m_t = freq_R_m_t
+        self.freq_l_m_t = freq_l_m_t
+        self.freq_j_m_t = freq_j_m_t
 
 
 class ActionSpace:
